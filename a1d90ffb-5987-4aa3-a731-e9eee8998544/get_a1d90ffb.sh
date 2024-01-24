834c8d97b09edcf1ebe26be72d5eb0451ce9711e
JavaScriptlee <155420066+JavaScriptlee@users.noreply.github.com>
2024-01-24 04:56:14 +0000

    Update and rename get_1fb9b714.sh to get_a1d90ffb.sh

--- a1d90ffb-5987-4aa3-a731-e9eee8998544/get_a1d90ffb.sh
+++ a1d90ffb-5987-4aa3-a731-e9eee8998544/get_a1d90ffb.sh
@@ -4,7 +4,7 @@ while [ "hello" = "hello" ]; do
     while IFS= read -r line; do
         curl -u admin:password "$line/start"
         sleep 5
-    done < "/users/a1d90ffb-5987-4aa3-a731-e9eee8998544/url_list.txt" 
+    done < "/workspace/users/a1d90ffb-5987-4aa3-a731-e9eee8998544/url_list.txt" 
     sleep 5m
 done
 echo Accidental termination
